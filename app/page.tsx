af5595d99ee165182b2781c1acf62d421ccc1f65
Pawel1894 <pawepohl@gmail.com>
2024-03-14 20:46:25 +0100

    removing testing drizzle stuff

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,7 @@
-import { db } from "../drizzle";
-import { users } from "../schema";
-import { Test } from "./Test";
-import { insertUser } from "./actions/user.actions";
-
 export default async function Home() {
-  const users2 = await db.select().from(users).all();
-
-  const onUserCreate = async (user: any) => {
-    'use server'
-    await insertUser({
-      email: user.email,
-      name: user.name
-    });
-  }
-
   return (
     <main>
-      <Test onUserCreate={onUserCreate}/>
-      <ul>
-        {users2.map((user) => (
-          <li key={user.id}>{user.name}</li>
-        ))}
-      </ul>
+
     </main>
   );
 }
